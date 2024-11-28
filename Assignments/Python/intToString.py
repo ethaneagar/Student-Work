2f9d902137a0430f17d204590dce749cc73f9997
ethaneagar <ethaneagar@yahoo.com>
2024-11-28 08:07:53 -0700

    Create intToString.py

---
+++ Assignments/Python/intToString.py
@@ -0,0 +1,22 @@
+
+# function: intToString 
+def intToString(integer, base):
+    # function: string representation of a number
+    returnStr = []
+    numBase = len(base)
+    
+    # while integer is not 0
+    while integer:
+    # start consuming the integer and get the remainder
+        integer, remainder = divmod(integer, numBase)
+        # lookup the remainder inside the base
+        returnStr.append(base[remainder])
+        # reverse the return string
+        returnStr.reverse()
+        # cleanup and return
+        return ''.join(returnStr)
+
+  # test function
+  expectEqual(intToString(1230,base10), '123O')
+  expectEqual(intToString(31337,base10), '31337')
+  expectEqual(intToString(123,base2), 'llllOll')
