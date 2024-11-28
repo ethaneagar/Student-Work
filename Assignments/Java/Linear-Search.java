9dcdbf62d1fe419b893e093381a093742e8bb3d3
Ethan Eagar <ethaneagar@yahoo.com>
2024-11-28 14:25:37 -0700

    Update Linear-Search.java
    cleaning formatting

--- Assignments/Java/Linear-Search.java
+++ Assignments/Java/Linear-Search.java
@@ -1,55 +1,52 @@
 
 
-// Linear Ssearch function
+import java.util.Scanner;
+
+// function: Linear Search
 public static int linearSearch(int[] haystack, int needle)
 {
-  for (int i = 0; i < haystack.length; i++)
-  {
-    // if found
-	  if (needle == haystack[i]) 
-    {
-      return i;
-    } // end if
-  } // end for
-
-  // otherwise
-  return -1;
+	for (int i = 0; i < haystack.length; i++)
+	{
+		// if found
+		if (needle == haystack[i]) 
+		{
+	      		return i;
+	    	} // end if
+	} // end for
+	// otherwise
+  	return -1;
 } // end function
-
 
 // main entry point
 static void main(String[] args) 
 {
-  // init vars
-  Scanner userInput = new Scanner(System.in);
-  int[] arrayToCheck = new int[100];
+	  // init vars
+	  Scanner userInput = new Scanner(System.in);
+	  int[] arrayToCheck = new int[100];
+	
+	  // get user input
+	  System.out.println("Enter values to search for between 0 and 99: ");
+	  int userValue = userInput.nextInt();
+	  
+	  // populate array with random values
+	  for (int i = 0; i < arrayToCheck.length; i++) 
+	  {
+		  arrayToCheck[i] = (int)(Math.random() * 100);
+	  } // end of for
+	
+	  // perform LinearSearch
+	  int foundResult = linearSearch(arrayToCheck, userValue);
 
-  // get user input
-  System.out.println("Enter values to search for between 0 and 99: ");
-  int userValue = userInput.nextInt();
-  
-  // populate array with random values
-  for (int i = 0; i < arrayToCheck.length; i++) 
-  {
-    arrayToCheck[i] = (int)(Math.random() * 100);
-  } // end of for
+	  // display results
+	  if (foundResult != -1) 
+	  {
+		System.out.println(userValue + " was found at location " + foundResult);
+	  }
+	  else
+	  {
+		System.out.println(userValue + " was not found in the array");			
+	  }
 
-  // perform LinearSearch
-  int foundResult = linearSearch(arrayToCheck, userValue);
-
-  // display results
-  if (foundResult != -1) 
-  {
-    System.out.println(userValue + " was found at location " + foundResult);
-  }
-  else
-  {
-    System.out.println(userValue + " was not found in the array");			
-  }
-
-  // debug print
-  // printArray("arrayToCheck", arrayToCheck);
-
-  // cleanup
+  	// cleanup
 	userInput.close();
 }
