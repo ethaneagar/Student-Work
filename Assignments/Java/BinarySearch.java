991d9561cdf78b56e5161405ca7b060883937a45
Ethan Eagar <ethaneagar@yahoo.com>
2024-11-28 16:41:14 -0700

    Update BinarySearch.java
    fixing formatting and comments

--- Assignments/Java/BinarySearch.java
+++ Assignments/Java/BinarySearch.java
@@ -1,27 +1,34 @@
-// simple example of a Binary Search within an array of random numbers
+// simple example of a binary search within an array of random numbers
 
 import java.util.Scanner;
 
-// function: binarySearch
-public static int binarySearch(int[] haystack, int needle) 
+// Function Description:
+// - performs binary search through an array
+// - start in the middle, only search first half or second half of array 
+// - use for large, sorted arrays
+// Arguments:
+// - searchArray - integer array to search through
+// - searchFor - integer value to search for
+// Return Value:
+// - When match is found, index is returned, otherwise -1 is returned
+public static int binarySearch(int[] searchArray, int searchFor) 
 {
-
 	int low = 0;						
-	int high = haystack.length - 1;			
+	int high = searchArray.length - 1;			
 	
-	// while index is below highest value of array 'haystack'
+	// while index is below highest value of array
 	while (high >= low) 
 	{
 		int middle = (low + high)/2;
-		if (needle < haystack[middle]) 
+		if (searchFor < searchArray[middle]) 
 		{
 			// start moving down
 			high = middle - 1;
 		}							
-		else if (needle == haystack[middle]) 
+		else if (searchFor == searchArray[middle]) 
 		{
 			// found
-		   	return middle;
+			return middle;
 		}
 		else 
 		{
@@ -71,4 +78,3 @@ public static void main(String[] args)
 	// cleanup
 	userInput.close();
 }
-	
