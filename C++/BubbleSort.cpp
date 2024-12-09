e0431f603be399ee4c0bcbdeb7688aade83634ca
Ethan Eagar <ethaneagar@yahoo.com>
2024-12-09 11:57:15 -0700

    Create BubbleSort.cpp
    init upload

---
+++ C++/BubbleSort.cpp
@@ -0,0 +1,58 @@
+// simple example of a bubble sort on an array
+
+#include <iostream>
+using namespace std;
+
+// Function Description:
+	// - performs bubble sort on an array
+	// - smallest values bubble to the top, largest values sink to the bottom
+	// Arguments:
+	// - unsortedArray - integer array to sort
+	// Return Value:
+	// - array passed by reference
+void bubbleSort(int* unsortedArray) {
+
+	int unsortedSize = sizeof(unsortedArray) / sizeof(unsortedArray[0]);
+
+	// outer loop controlling the number of sort passes
+	for (int arrayItem = 1; arrayItem < unsortedSize; arrayItem++) {
+		// inner loop controlling the number of comparisons 
+		for (int compareItem = 0; compareItem < unsortedSize - 1; compareItem++) {
+			// swap if out of order
+			if (unsortedArray[compareItem] > unsortedArray[compareItem + 1]) {
+				int swapItem = unsortedArray[compareItem];
+				unsortedArray[compareItem] = unsortedArray[compareItem + 1];
+				unsortedArray[compareItem + 1] = swapItem;
+			} // end if out of order
+		} // end for each comparison
+	} // end for each array item
+} // bubble sort
+
+// main entry point
+int main() {
+	int arrayToSort[20];
+	int arraySize = sizeof(arrayToSort) / sizeof(arrayToSort[0]);
+
+	// populate array with random values
+	for (int index = 0; index < arraySize; index++)
+	{
+		arrayToSort[index] = (int) rand() % 100;
+	} // end of for	
+
+	std::cout << "Array prior to sort: \n";
+	for (int index = 0; index < arraySize; index++)
+	{
+		std::cout << arrayToSort[index];
+	}
+
+	// perform bubble sort
+	bubbleSort(arrayToSort);
+
+	std::cout << "\n\nArray after sort: \n";
+	for (int index = 0; index < arraySize; index++)
+	{
+		std::cout << arrayToSort[index];
+	}
+
+	std::cout << "\n";
+} // main
